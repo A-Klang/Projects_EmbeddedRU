044b2e61b69ca33f6c00e1b4c2c56709ccb060f6
A-Klang <103817500+A-Klang@users.noreply.github.com>
2026-08-27 19:44:59 +0000

    Updated main.cpp for clarity

--- Project_1/src/main.cpp
+++ Project_1/src/main.cpp
@@ -1,14 +1,17 @@
-#include <Arduino.h>
 #include <util/delay.h>
 #include <encoder.h>
 
-Encoder enc(3, 4, 5); // C1=PD3, C2=PD4, LED = PB5)
+int main()
+{
+  Encoder enc(3, 4, 5); // C1=PD3, C2=PD4, LED=PB5
 
-void setup() {
   enc.init();
-}
 
-void loop() {
-  enc.sample();
-  _delay_us(140); // ~286/2
+  while (1)
+  {
+    enc.sample();
+    _delay_us(140); // ~286/2
+  }
+
+  return 0;
 }
