47813ac0784342a24d1dc87e868a0bc7ea2535ad
A-Klang <103817500+A-Klang@users.noreply.github.com>
2026-09-04 14:47:52 +0000

    Moved interrupt into encoder.cpp. Solved by moving all things related to encoder object into encoder.cpp.

--- Project_1 parr 3/include/encoder.h
+++ Project_1 parr 3/include/encoder.h
@@ -6,7 +6,7 @@ class Encoder
     public:
         Encoder(uint8_t c1_pin, uint8_t c2_pin, uint8_t led_pin);
         void init();
-        void sample();
+        void update(); // called from ISR(PCINT2_vect), not meant to be called directly
         int position();
 
     private:
@@ -14,5 +14,10 @@ class Encoder
         Digital_in c2;
         Digital_out led;
         bool last_c1;
-        int pos;
+        volatile int pos;
 };
+
+// The single encoder instance. Defined in encoder.cpp (pin wiring lives
+// there, next to the interrupt setup it belongs with) so ISR(PCINT2_vect)
+// can reach it directly by name, with no runtime pointer indirection.
+extern Encoder enc;
--- Project_1 parr 3/src/main.cpp
+++ Project_1 parr 3/src/main.cpp
@@ -2,22 +2,18 @@
 #include <encoder.h>
 #include <Arduino.h>
 
+
 int main()
 {
-  // Serial.begin(9600);
-  //sei(); // Enable UART interrupts used by Serial.
-  Encoder enc(4, 3, 5); // C2=PD3, C1=PD4, LED=PB5
-
+  Serial.begin(115200);
   enc.init();
-
+  
+  sei();
   while (1)
   {
-    enc.sample();
-
-    //Serial.println(enc.position());
-
-    _delay_us(140); // ~286/2
+    Serial.println(enc.position());
   }
 
   return 0;
 }
+
