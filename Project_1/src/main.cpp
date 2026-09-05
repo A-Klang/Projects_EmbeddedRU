a83172e8a609bcd53ea9c67cf37e26734b9b9ec1
carter-gibson <carter.mac.gibson@gmail.com>
2026-09-05 21:32:37 +0000

    Enable Serial communication and print encoder position

--- Project_1/src/main.cpp
+++ Project_1/src/main.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-  // Serial.begin(9600);
-  //sei(); // Enable UART interrupts used by Serial.
+  Serial.begin(9600);
+  sei(); // Enable UART interrupts used by Serial.
   Encoder enc(4, 3, 5); // C2=PD3, C1=PD4, LED=PB5
 
   enc.init();
@@ -14,7 +14,7 @@ int main()
   {
     enc.sample();
 
-    //Serial.println(enc.position());
+    Serial.println(enc.position());
 
     _delay_us(140); // ~286/2
   }
